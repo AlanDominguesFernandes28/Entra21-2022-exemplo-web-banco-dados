97e91ffdd3e85f8094ca2559d7ddd176ff6fafc2
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-08-03 22:11:17 -0300

    Aula HTML e exemplo

---
+++ Entra21.CSharp.ClinicaVterinarai/WebApplication1/Controllers/RacaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
+{
+    public class RacaController : Controller
+    {
+        /// <summary>
+        /// EndPoint que permite listar toda as racas
+        /// </summary>
+        /// <returns></returns>Retorna a pagina html com as racas</returns>
+        [Route("/raca")]
+        [HttpGet]
+
+        public IActionResult ObterTodos()
+        {
+            return View("Index");
+        }
+        [Route("/raca/Cadastrar")]
+        [HttpGet]
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [Route("/raca/Registrar")]
+        [HttpGet]
+
+        public IActionResult Registrar(
+            [FromQuery] string nome,
+            [FromQuery] string especie)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
